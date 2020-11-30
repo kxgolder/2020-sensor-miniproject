0afe078de88e7f11828c7df6dfe5b93c5fb3292e
kxgolder <55505652+kxgolder@users.noreply.github.com>
2020-11-29 20:58:46 -0500

    Update client.py

--- src/sp_iotsim/client.py
+++ src/sp_iotsim/client.py
@@ -46,11 +46,14 @@ async def main(port: int, addr: str, max_packets: int, log_file: Path = None):
 
         if log_file:
             log_file = Path(log_file).expanduser()
+            file = log_file.open("a")
             raise NotImplementedError("The code to open the file should be here.")
 
         for _ in range(max_packets):
             data = await websocket.recv()
             print(data)
+            file.write(txt + "\n")
+            
 
 
 def cli():
